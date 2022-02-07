fc846b9a3a04761c69c85747f8228bf448cafa48
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-06 18:51:03 -0600

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 var myName = "Paul";
 var lastName = "Anigbo";
 var luckyNumber = 7;
+var date = new Date();
 
 ReactDOM.render(
   <div>
-    <h1>Welcome back {`${myName} ${lastName}`}</h1>
-    <p>Your lucky number is {luckyNumber}</p>
+    <p>created by {`${myName} ${lastName}`}</p>
+    <p>copyright {`${date.getFullYear()}`}</p>
   </div>,
   document.getElementById("root")
 );
